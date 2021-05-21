1d6a65645e91d05a439d8283270d16da12f8bebe
BuildTools <unconfigured@null.spigotmc.org>
2021-05-21 19:27:54 +0200

    updates version alpha0.0.4.1

---
+++ src/main/java/charlsen/charlsens/ideas/tools/BorniteSwordItem.java
@@ -0,0 +1,10 @@
+package charlsen.charlsens.ideas.tools;
+
+import net.minecraft.item.SwordItem;
+import net.minecraft.item.ToolMaterial;
+
+public class BorniteSwordItem extends SwordItem {
+    public BorniteSwordItem(ToolMaterial toolMaterial, int attackDamage, float attackSpeed, Settings settings) {
+        super(toolMaterial, attackDamage, attackSpeed, settings);
+    }
+}
