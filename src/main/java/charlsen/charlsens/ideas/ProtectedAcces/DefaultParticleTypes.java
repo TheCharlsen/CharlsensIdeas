2c86a31853d5c4456774ad457262a979ed731361
BuildTools <unconfigured@null.spigotmc.org>
2021-06-22 17:05:28 +0200

    First 1.17 release

---
+++ src/main/java/charlsen/charlsens/ideas/ProtectedAcces/DefaultParticleTypes.java
@@ -0,0 +1,9 @@
+package charlsen.charlsens.ideas.ProtectedAcces;
+
+import net.minecraft.particle.DefaultParticleType;
+
+public class DefaultParticleTypes extends DefaultParticleType {
+    public DefaultParticleTypes(boolean alwaysShow) {
+        super(alwaysShow);
+    }
+}
--- src/main/java/charlsen/charlsens/ideas/World/CharlsensideasStructureFeature.java
+++ src/main/java/charlsen/charlsens/ideas/World/CharlsensideasStructureFeature.java
@@ -1,16 +1,20 @@
 package charlsen.charlsens.ideas.World;
 
-import charlsen.charlsens.ideas.Generators.Structures.CHIDStructure;
-import charlsen.charlsens.ideas.Utils.RegUtils;
-import charlsen.charlsens.ideas.Utils.StructureUtils;
-import net.minecraft.world.gen.feature.StructureFeature;
-import net.minecraft.world.gen.feature.StructurePoolFeatureConfig;
+
+import charlsen.charlsens.ideas.Charlsensideas;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.BuiltinRegistries;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.world.gen.feature.ConfiguredStructureFeature;
+import net.minecraft.world.gen.feature.DefaultFeatureConfig;
 
 public class CharlsensideasStructureFeature {
 
-    public static final StructureFeature<StructurePoolFeatureConfig> BARN_HOUSE_SF = new CHIDStructure();
+        public static ConfiguredStructureFeature<?, ?> CONFIGURED_DIAMOND_CASCADE = CharlsensideasStructures.DIAMOND_CASCADE.configure(DefaultFeatureConfig.DEFAULT);
 
-    public static void registerStructures() {
-        RegUtils.registerStructure(StructureUtils.BARN_HOUSE, BARN_HOUSE_SF, CharlsensideasConfiguredFeatures.BARN_HOUSE);
+    public static void registerConfiguredStructures() {
+
+        Registry<ConfiguredStructureFeature<?, ?>> registry = BuiltinRegistries.CONFIGURED_STRUCTURE_FEATURE;
+        Registry.register(registry, new Identifier(Charlsensideas.MODID, "configured_diamond_cascade"), CONFIGURED_DIAMOND_CASCADE);
     }
 }
