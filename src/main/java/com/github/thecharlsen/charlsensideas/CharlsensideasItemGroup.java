c4da607f6a8eff40af52409f73f12664ecd1fd11
BuildTools <unconfigured@null.spigotmc.org>
2021-09-13 16:10:07 +0200

    Beta 0.2.2

--- src/main/java/com/github/thecharlsen/charlsensideas/CharlsensideasItemGroup.java
+++ src/main/java/com/github/thecharlsen/charlsensideas/CharlsensideasItemGroup.java
@@ -7,34 +7,34 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.util.Identifier;
 
 public class CharlsensideasItemGroup implements ModInitializer {
-    
-    public static final ItemGroup ITEM_GROUP_SECCHESTS = FabricItemGroupBuilder.build(
-		new Identifier("charlsensideas", "secchests"),
-		() -> new ItemStack(CharlsensideasItems.Secure_Chest_Module));
 
-			public static final ItemGroup ITEM_GROUP_FOOD = FabricItemGroupBuilder.build(
-			new Identifier("charlsensideas", "food"),
-			() -> new ItemStack(CharlsensideasBlocks.Alpine_Strawberry_Cake));
+			public static final ItemGroup ITEM_GROUP_NATURE = FabricItemGroupBuilder.build(
+			new Identifier("charlsensideas", "nature"),
+			() -> new ItemStack(CharlsensideasBlocks.MuddedDirt));
+
+			public static final ItemGroup ITEM_GROUP_BUILDING_BLOCKS = FabricItemGroupBuilder.build(
+			new Identifier("charlsensideas", "building_blocks"),
+			() -> new ItemStack(CharlsensideasBlocks.Chiseled_Black_Tourmaline_Stone));
 
 			public static final ItemGroup ITEM_GROUP_ORES = FabricItemGroupBuilder.build(
 			new Identifier("charlsensideas", "ores"),
 			() -> new ItemStack(CharlsensideasItems.Bornite));
 
+			public static final ItemGroup ITEM_GROUP_TOOLS = FabricItemGroupBuilder.build(
+			new Identifier("charlsensideas", "tools"),
+			() -> new ItemStack(CharlsensideasTools.Bornite_Pickaxe));
+
+			public static final ItemGroup ITEM_GROUP_FOOD = FabricItemGroupBuilder.build(
+			new Identifier("charlsensideas", "food"),
+			() -> new ItemStack(CharlsensideasBlocks.Alpine_Strawberry_Cake));
+
 			public static final ItemGroup ITEM_GROUP_MUSIC = FabricItemGroupBuilder.build(
 				new Identifier("charlsensideas", "music"),
 				() -> new ItemStack(CharlsensIdeasClientModInitializer.MUSICPLAYER));
 
-	        public static final ItemGroup ITEM_GROUP_NATURE = FabricItemGroupBuilder.build(
-			new Identifier("charlsensideas", "nature"),
-			() -> new ItemStack(CharlsensideasBlocks.MuddedDirt));
-
-	        public static final ItemGroup ITEM_GROUP_BUILDING_BLOCKS = FabricItemGroupBuilder.build(
-			new Identifier("charlsensideas", "building_blocks"),
-			() -> new ItemStack(CharlsensideasBlocks.Chiseled_Black_Tourmaline_Stone));
-
-	        public static final ItemGroup ITEM_GROUP_TOOLS = FabricItemGroupBuilder.build(
-			new Identifier("charlsensideas", "tools"),
-			() -> new ItemStack(CharlsensideasTools.Bornite_Pickaxe));
+			public static final ItemGroup ITEM_GROUP_SECCHESTS = FabricItemGroupBuilder.build(
+			new Identifier("charlsensideas", "secchests"),
+			() -> new ItemStack(CharlsensideasItems.Secure_Chest_Module));
 
 	        @Override
 			public void onInitialize() {
