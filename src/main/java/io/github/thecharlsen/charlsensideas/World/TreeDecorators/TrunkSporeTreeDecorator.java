8178b279982feb930c6e0337d5cc2ded90a76197
TheCharlsen <charlie.saeck@icloud.com>
2021-11-02 18:07:28 +0100

    21dev-build0211

--- src/main/java/io/github/thecharlsen/charlsensideas/World/TreeDecorators/TrunkSporeTreeDecorator.java
+++ src/main/java/io/github/thecharlsen/charlsensideas/World/TreeDecorators/TrunkSporeTreeDecorator.java
@@ -10,20 +10,19 @@ import net.minecraft.block.VineBlock;
 import net.minecraft.state.property.BooleanProperty;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
+import net.minecraft.world.ChunkRegion;
 import net.minecraft.world.TestableWorld;
 import net.minecraft.world.gen.feature.Feature;
 import net.minecraft.world.gen.treedecorator.CocoaBeansTreeDecorator;
 import net.minecraft.world.gen.treedecorator.TreeDecorator;
 import net.minecraft.world.gen.treedecorator.TreeDecoratorType;
-import net.minecraft.world.gen.treedecorator.TrunkVineTreeDecorator;
 
-import java.util.Iterator;
 import java.util.List;
 import java.util.Random;
 import java.util.function.BiConsumer;
 
 public class TrunkSporeTreeDecorator extends TreeDecorator {
-    public static final Codec<TrunkSporeTreeDecorator> CODEC = Codec.floatRange(0.0F, 1.0F).fieldOf("probability").xmap(TrunkSporeTreeDecorator::new, (decorator) -> {
+    public static final Codec<TrunkSporeTreeDecorator> CODEC = Codec.floatRange(0.0F, 0.05F).fieldOf("probability").xmap(TrunkSporeTreeDecorator::new, (decorator) -> {
         return decorator.probability;
     }).codec();
     private final float probability;
@@ -36,25 +35,16 @@ public class TrunkSporeTreeDecorator extends TreeDecorator {
         return ChidTreeDecoratorTypes.SPORE_TRUNK;
     }
 
+    @Override
     public void generate(TestableWorld world, BiConsumer<BlockPos, BlockState> replacer, Random random, List<BlockPos> logPositions, List<BlockPos> leavesPositions) {
-        if (!(random.nextFloat() >= this.probability)) {
-            int i = ((BlockPos)logPositions.get(0)).getY();
-            logPositions.stream().filter((pos) -> {
-                return pos.getY() - i <= 2;
-            }).forEach((pos) -> {
-                Iterator var4 = Direction.Type.HORIZONTAL.iterator();
+        ChunkRegion world1 = (ChunkRegion) world;
 
-                while(var4.hasNext()) {
-                    Direction direction = (Direction)var4.next();
-                    if (random.nextFloat() <= 0.25F) {
-                        Direction direction2 = direction.getOpposite();
-                        BlockPos blockPos = pos.add(direction2.getOffsetX(), 0, direction2.getOffsetZ());
-                        if (Feature.isAir(world, blockPos)) {
-                            replacer.accept(blockPos, (BlockState)((BlockState) CharlsensideasBlocks.Spore.getDefaultState()).with(SporeWallBlock.FACING, direction));
-                        }
-                    }
-                }
-            });
+        for (BlockPos pos : logPositions) {
+            Direction dir = Direction.fromHorizontal(random.nextInt(4));
+
+            if (world1.getBlockState(pos.offset(dir)).isAir()) {
+                world1.setBlockState(pos.offset(dir), CharlsensideasBlocks.Spore.getDefaultState().with(SporeWallBlock.FACING, dir), 3);
+            }
         }
     }
 }
